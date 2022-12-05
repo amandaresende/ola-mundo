7d10233cfee6112a45f1d47c7706941e291b2c26
Amanda Resende <amandacamposresende@gmail.com>
2022-12-05 09:30:30 -0300

    Feat: Finalização do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,27 @@
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
+import Post from "paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
+import NaoEncontrada from './paginas/NãoEncontrada';
+import ScrollToTop from "componentes/ScrollToTop";
+
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Menu />
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
-        </Route>
-        
-        <Route path="*" element={<div>Página não encontrada</div>} />
+               </Route>
+               <Route path="posts/:id" element={<Post/>}/>
+        <Route path="*" element={<NaoEncontrada/>} />
       </Routes>
 
       <Rodape />
